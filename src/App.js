97cc1e32f760fb38f2d28e58ab6ad9da916f97cc
Leenamkwon <namkwon12@gmail.com>
2020-04-27 16:04:49 +0900

    learned:ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import Validation from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
-    return <Validation />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 
