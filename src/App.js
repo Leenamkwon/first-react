313192966f0d8318ab6d3c8567d9868ce31da901
Leenamkwon <namkwon12@gmail.com>
2020-04-28 14:28:59 +0900

    learned: LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-import Iteration from './iterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.state({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <Iteration />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
---
+++ src/LifeCycleSample.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+class LifeCycleSample extends Component {
+  state = {
+    number: 0,
+    color: null,
+  };
+
+  myRef = null; // ref를 설정할 부분
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  // 부모에게 받은 props를 state에 동기화 작업
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('getDerivedStateFromProps');
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  // 렌더링이 끝난 뒤
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate', nextProps, nextState);
+    // 숫자의 마지막 자리가 4면 리렌더링하지 않습니다.
+    return nextState.number % 10 !== 4;
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  // DOM에서 변화가 일어나기 직전의 색상 속성을 snapshot 값으로 반환
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  // getSnapshotBeforeUpdate의 snapshot을 조회
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate', prevProps, prevState);
+    if (snapshot) {
+      console.log('업데이트되기 직전 색상: ', snapshot);
+    }
+  }
+
+  render() {
+    console.log('render');
+    const style = {
+      color: this.props.color,
+    };
+    return (
+      <div>
+        {/* {this.props.missing.value} */}
+        <h1 style={style} ref={(ref) => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>더하기</button>
+      </div>
+    );
+  }
+}
+export default LifeCycleSample;
