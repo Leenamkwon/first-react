c3725676dde8a5a3236b6872bfa2d38b3f0830c0
Leenamkwon <namkwon12@gmail.com>
2020-05-04 03:38:48 +0900

    learned: CSS module

---
+++ src/CSSModule.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './CSSModule.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      안녕하세요, 저는 <span className="something">CSS module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
